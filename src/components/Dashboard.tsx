bb50d9daed34bc67614102fd5c38a6f460925e14
gtarragona <gtarragona@switchfly.com>
2020-12-30 21:43:23 -0700

    Adding reducer and saga files for Metrics functionality. Adding mock data for dashboard.

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,15 +1,64 @@
-import { Box, Container } from '@material-ui/core';
+import { Box, Card, CardContent, Container, Grid, Typography } from '@material-ui/core';
 import React from 'react';
 import MultipleSelect from './MultipleSelect';
 
+interface Metric {
+  metric: string;
+  value: number;
+  unit: string;
+}
+
 export default () => {
   const metrics = ['flareTemp', 'injValveOpen', 'oilTemp', 'casingPressure', 'tubingPressure', 'waterTemp'];
+
+  const selectedValuesHandler = (values: string[]) => {
+    console.log('values from dashboard', values);
+  };
+
+  const selectedMetris: Metric[] = [
+    {
+      metric: 'oilTemp',
+      value: 96.08,
+      unit: 'F',
+    },
+    {
+      metric: 'anotherTemp',
+      value: 96.08,
+      unit: 'F',
+    },
+    {
+      metric: 'waterTemp',
+      value: 96.08,
+      unit: 'F',
+    },
+  ];
   return (
     <Container maxWidth="lg">
-      {/* <Box></Box> */}
-      <Box>
-        <MultipleSelect values={metrics} />
-      </Box>
+      <Grid direction="row" spacing={2} container>
+        <Grid md={8} item>
+          <Grid container spacing={2}>
+            {selectedMetris.map((metric) => {
+              return (
+                <Grid key={metric.metric} xs={6} item>
+                  <Card>
+                    <CardContent>
+                      <CardContent>
+                        <Typography>{metric.metric}</Typography>
+                        <Typography>{metric.value}</Typography>
+                      </CardContent>
+                    </CardContent>
+                  </Card>
+                </Grid>
+              );
+            })}
+          </Grid>
+        </Grid>
+        <Grid xs={12} sm={12} md={4} item>
+          <Box display="flex" flexDirection="row" justifyContent="flex-end">
+            <MultipleSelect values={metrics} changeCallback={selectedValuesHandler} />
+          </Box>
+        </Grid>
+      </Grid>
     </Container>
   );
 };
--- src/components/MultipleSelect.tsx
+++ src/components/MultipleSelect.tsx
@@ -1,22 +1,32 @@
-import { InputLabel, FormControl, Select, Input, MenuItem, makeStyles } from '@material-ui/core';
+import { InputLabel, FormControl, Select, Input, MenuItem, makeStyles, Chip } from '@material-ui/core';
 import React, { useState } from 'react';
 
 type MultipleSelectProps = {
   values: string[];
+  changeCallback: Function;
 };
 
 const useStyles = makeStyles({
   formControl: {
-    minWidth: 120,
+    minWidth: 180,
+    backgroundColor: 'white',
+  },
+  chips: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  chip: {
+    margin: 2,
   },
 });
 
-const MultipleSelect = ({ values = ['No metrics'] }: MultipleSelectProps) => {
+const MultipleSelect = ({ values = ['No metrics'], changeCallback }: MultipleSelectProps) => {
   const [multipleSelectValue, setMultipleSelectValue] = useState<string[]>([]);
   const classes = useStyles();
 
   const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
     setMultipleSelectValue(event.target.value as string[]);
+    changeCallback(event.target.value as string[]);
   };
 
   return (
@@ -29,6 +39,13 @@ const MultipleSelect = ({ values = ['No metrics'] }: MultipleSelectProps) => {
         value={multipleSelectValue}
         onChange={handleChange}
         input={<Input />}
+        renderValue={(selected) => (
+          <div className={classes.chips}>
+            {(selected as string[]).map((value) => (
+              <Chip key={value} label={value} className={classes.chip} />
+            ))}
+          </div>
+        )}
       >
         {values.map((value: string) => (
           <MenuItem key={value} value={value}>
